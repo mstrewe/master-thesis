44fd1a2daf1518f6910bb6f305f950761d97b0d1
Maik Piel <strewe@uni-potsdam.de>
2017-12-03 13:40:13 +0000

    add players and enable easyrtc on same app

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var http = require('http');
 var apiMiddleWare = require('./src/api-middleware.js');
 // gzip/deflate outgoing responses
 var compression = require('compression');
-var serveStatic = require('serve-static')
+var serveStatic = require('serve-static');
+//easyrtc components
+var easyrtc = require('easyrtc');
+var socketIo = require("socket.io");  
+var express = require("express");
 
 // create route handlers
 var serveEasyRTC = serveStatic('easyrtc', {
@@ -42,3 +46,19 @@ app.use('/', serveIndex);
 
 //create node.js http server and listen on port
 http.createServer(app).listen(8000);
+console.log("Webserver started at 8000.");
+
+
+//initialize easyrtc 
+// Setup and configure Express http server. Expect a subfolder called "static" to be the web root.
+var httpApp = express();
+httpApp.use(express.static(__dirname));
+
+// Start Express http server on port 8080
+var webServer = http.createServer(httpApp).listen(8080);
+
+// Start Socket.io so it attaches itself to Express server
+var socketServer = socketIo.listen(webServer);
+
+// Start EasyRTC server
+var easyrtcServer = easyrtc.listen(httpApp, socketServer);
