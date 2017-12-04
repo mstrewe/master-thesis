90cf7a6dcce50e952492c3dbc064a26fb4c7ac47
Maik Piel <mstrewe@googlemail.com>
2017-12-04 21:50:25 +0000

    add buttons to load other pdfs

--- resources/js/pdf-loader.js
+++ resources/js/pdf-loader.js
@@ -1,12 +1,15 @@
 var pdfLoader =
     {
-        init: function () {
-            // URL of PDF document
-            var url = "resources/pdf/143-1-611-1-10-20080609.pdf";
+        documentCache: { url: {}, pdf: {}, page: 0 },
+
+        loadUrl: function (url) {
 
             // Asynchronous download PDF
             PDFJS.getDocument(url)
                 .then(function (pdf) {
+                    pdfLoader.documentCache.url = url;
+                    pdfLoader.documentCache.pdf = pdf;
+                    pdfLoader.documentCache.page = 1;
                     return pdf.getPage(1);
                 })
                 .then(function (page) {
@@ -35,7 +38,83 @@ var pdfLoader =
                     // Render PDF page
                     page.render(renderContext);
                 });
+        },
+
+
+        nextPage: function () {
+            var page = pdfLoader.documentCache.pdf.getPage(pdfLoader.documentCache.page + 1);
+            // Set scale (zoom) level
+            var scale = 1.5;
+
+            // Get viewport (dimensions)
+            var viewport = page.getViewport(scale);
+
+            // Get canvas#the-canvas
+            var canvas = document.getElementById('the-canvas');
+
+            // Fetch canvas' 2d context
+            var context = canvas.getContext('2d');
+
+            // Set dimensions to Canvas
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Prepare object needed by render method
+            var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+
+            // Render PDF page
+            page.render(renderContext);
+        },
+
+        previusPage: function () {
+            if (pdfLoader.documentCache.page - 1 === 0)
+                return;
+            var page = documentCache.pdf.getPage(pdfLoader.documentCache.page - 1);
+            // Set scale (zoom) level
+            var scale = 1.5;
+
+            // Get viewport (dimensions)
+            var viewport = page.getViewport(scale);
+
+            // Get canvas#the-canvas
+            var canvas = document.getElementById('the-canvas');
+
+            // Fetch canvas' 2d context
+            var context = canvas.getContext('2d');
+
+            // Set dimensions to Canvas
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Prepare object needed by render method
+            var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+
+            // Render PDF page
+            page.render(renderContext);
         }
     };
 
-pdfLoader.init();
+window.onload = function () {
+    document.getElementById('pdf_next_page').addEventListener('click', function (evt) {
+        pdfLoader.nextPage();
+        console.log('Next page clicked: ', evt.detail.intersection.point);
+    });
+    document.getElementById('pdf_previus_page').addEventListener('click', function (evt) {
+        pdfLoader.previusPage();
+        console.log('Previus page clicked: ', evt.detail.intersection.point);
+    });
+    document.getElementById('pdf1').addEventListener('click', function (evt) {
+        pdfLoader.loadUrl("resources/pdf/02_Mobile_Learning.pdf");
+        console.log('pdf1 clicked: ', evt.detail.intersection.point);
+    });
+    document.getElementById('pdf2').addEventListener('click', function (evt) {
+        pdfLoader.loadUrl("resources/pdf/143-1-611-1-10-20080609.pdf");
+        console.log('pdf2 clicked: ', evt.detail.intersection.point);
+    });
+};
