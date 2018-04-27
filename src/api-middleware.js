f9775f02d55c23227ce3a0356e93b4be34edd217
Maik Piel <mstrewe@googlemail.com>
2018-04-27 21:49:23 +0100

    remove files when teacher logs in

--- src/api-middleware.js
+++ src/api-middleware.js
@@ -11,16 +11,21 @@ module.exports = {
                 var cookie = self.getNewCookie();
                 res.cookie('teacher_login_cookie', cookie, { maxAge: 7200000 });
                 self._currentCookie = cookie;
+                var fileSystem = require("fs");
+                var files = fileSystem.readdirSync("public/uploads");
+                files.forEach(element => {
+                    fileSystem.unlinkSync("public/uploads/" + element);
+                });
             }
         }
 
         if (req._parsedUrl.pathname == "/api/get_files" && req.method == "GET") {
             if (req.cookies.teacher_login_cookie != self.getCurrentCookie())
                 res.sendStatus(401);
-           var fileSystem = require("fs");
-           var files = fileSystem.readdirSync("public/uploads");
+            var fileSystem = require("fs");
+            var files = fileSystem.readdirSync("public/uploads");
 
-           responseObject = files;
+            responseObject = files;
         }
 
         res.status(200).json(responseObject);
